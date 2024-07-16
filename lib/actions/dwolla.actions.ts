ecd5eac6688139a76ef8ed2d72631586a3e0b3e2
sirAges <ekele19stephen96u@gmail.com>
2024-07-16 08:11:25 +0100

    final commit

--- lib/actions/dwolla.actions.ts
+++ lib/actions/dwolla.actions.ts
@@ -59,7 +59,7 @@ export const createDwollaCustomer = async (
       .post("customers", newCustomer)
       .then((res) => res.headers.get("location"));
   } catch (err) {
-    console.error("Creating a Dwolla Customer Failed: ", err);
+    console.error("Creating a Dwolla Customer Failed: ", err.message);
   }
 };
 
